42afd6de0a11552f224c08b04cbcf295f02a716e
Joel <joel@nve.no>
2025-05-09 15:22:29 +0200

    fix

--- src/components/NveTable.vue
+++ src/components/NveTable.vue
@@ -216,6 +216,7 @@ const observer = new IntersectionObserver(
   ([e]) => e.target.toggleAttribute("data-stuck", e.intersectionRatio < 1),
   { threshold: [1] }
 );
+
 const tableheader: Ref<HTMLTableSectionElement | null> = ref(null);
 onMounted(() => {
   const thead = tableheader.value;
